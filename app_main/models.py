4dc022dc4e981ee861fab86ed0d4107761024f2d
kir <twst@test.com>
2020-05-01 21:30:29 +0400

    create default project

---
+++ app_main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.CharField("User's Email", max_length=200, unique=True)
+      
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email
+        
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
