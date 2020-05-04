96d787d619516b1da59b7479f560505a48089dc7
Max <Max>
2020-05-04 23:24:36 +0300

    Template was created

--- app_main/models.py
+++ app_main/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 from django.utils.translation import ugettext_lazy as _
 from .managers import CustomUserManager
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 
 class Substation(models.Model):
     name = models.CharField("Название организации", max_length=200, default='')
