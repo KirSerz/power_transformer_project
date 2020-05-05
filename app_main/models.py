83cb4231208b2b0ad523a00f6f7f61a7fe27467c
kir <twst@test.com>
2020-05-05 17:38:26 +0400

    fix form

--- app_main/models.py
+++ app_main/models.py
@@ -7,11 +7,11 @@ from django.db import models
 from django.utils import timezone
 
 class Substation(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
     name = models.CharField("Название организации", max_length=200, default='')
     city = models.CharField("Город", max_length=200, default='')
     street = models.CharField("Улица", max_length=200, default='')
     descriptions = models.CharField("Описание", max_length=200, default='')
-    date = models.DateTimeField('Дата изменениея баланса', auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -34,15 +34,27 @@ class User(AbstractUser):
         swappable = 'AUTH_USER_MODEL'
 
 class Transformer(models.Model):
+    """добавить спецификации трансформатора"""
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transformer_ID = models.CharField("Transformer's ID", max_length=200, unique=True)
-    location = models.CharField("Location", max_length=200, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    substation = models.ForeignKey(Substation, on_delete=models.CASCADE)
+    unique_key = models.CharField("Transformer's ID", max_length=200, default='')
+    descriptions = models.TextField("Описание", max_length=200, default='')
+
+    def __str__(self):
+        return '{0} {1}'.format(self.unique_key, self.substation)
+    
+    class Meta:
+	    verbose_name_plural = "Трансформатор"
 
 class DataDGA(models.Model):
-    
+    date = models.DateTimeField(auto_now_add=True)
     transformer = models.ForeignKey(Transformer, on_delete=models.CASCADE)
     data_dga = models.TextField('Данные DGA(json)', blank=True, default='')
-    classification_score = models.FloatField('Оценка классификатора', default=0)   
+    classification_score = models.FloatField('Оценка классификатора', blank=True, null=True, default=None)
     
+    def __str__(self):
+        return '{0}'.format(self.classification_score)
     
+    class Meta:
+	    verbose_name_plural = "Данные DGA"
