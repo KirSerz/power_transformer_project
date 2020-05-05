fd63cc9b97d926193c30473fe85d0081b501f025
Max <Max>
2020-05-05 16:47:20 +0300

    update Please enter the commit message for your changes. Lines starting

--- app_main/views.py
+++ app_main/views.py
@@ -6,8 +6,25 @@ from .models import DataDGA
 
 
 def post_list(request):
+    id_substation=1
+    context={}
+    try:
+        substation=Substation.objects.get(id=id_substation)
+        context["substation"]=substation
+        transformers=Transformer.objects.filter(substation=substation.id)
+        transformer_data = {item.id: {
+            "transformer":item,
+            'data':list()
+        } for item in transformers}
+    except:
+        transformers=[]
+        transformer_data={}
     
-    return render(request, 'post_list.html', {})
+    data_DGA=DataDGA.objects.filter(transformer__in=transformers).order_by('date')
+    for dga in data_DGA:
+        transformer_data[dga.transformer.id]["data"].append(dga)
+    context["data_transformers"]=transformer_data.values()
+    return render(request, 'post_list.html', context)
 
 
 from django.http import HttpResponse, JsonResponse
