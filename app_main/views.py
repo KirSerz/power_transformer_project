2e79c170a972398fc4ccd7c9bec1c6062be9e2fa
kir <twst@test.com>
2020-05-05 18:05:37 +0400

    Add upload_data procedure in Transformer model

--- app_main/views.py
+++ app_main/views.py
@@ -6,8 +6,25 @@ from .models import DataDGA
 
 
 def post_list(request):
+    id_substation=1
+    context={}
+    try:
+        substation=Substation.objects.get(id=id_substation)
+        context["substation"]=substation
+        transformers=Transformer.objects.filter(substation=substation.id)
+        transformer_data = {item.id: {
+            "transformer":item,
+            'data':list()
+        } for item in transformers}
+    except:
+        transformers=[]
+        transformer_data={}
     
-    return render(request, 'post_list.html', {})
+    data_DGA=DataDGA.objects.filter(transformer__in=transformers).order_by('date')
+    for dga in data_DGA:
+        transformer_data[dga.transformer.id]["data"].append(dga)
+    context["data_transformers"]=transformer_data.values()
+    return render(request, 'post_list.html', context)
 
 
 from django.http import HttpResponse, JsonResponse
@@ -18,10 +35,39 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 
-class BasePage(View):
-    def get(self, request, *args, **kwargs):
+def post_list(request):
+    
+    return render(request, 'post_list.html', {})
+
+class ApiAddDataDGA(View):
+    def post(self, request, *args, **kwargs):
         context = {}
         context['status'] = 'ok'
+        context['message'] = ''
+        id_transformer = request.POST.get('unique_key')
+        
+        # TODO add model forms, validation form
+
+        try:
+            transformer = Transformer.objects.get(unique_key = unique_key)
+        except:
+            transformer = None
+
+        if id_transformer:
+            new_data_dga = transformer.upload_data(
+                classification_score = None, 
+                data_dga = request.POST.get('data_dga')
+            )
+        else:
+            context['status'] = 'error'
+            context['message'] = 'Invalid id transformer'
+        
+        return JsonResponse(context)
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['status'] = 'error'
+        context['message'] = 'Invalid id method'
         return JsonResponse(context)
 
 
